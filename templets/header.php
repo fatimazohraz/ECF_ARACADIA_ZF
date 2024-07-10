8a001044a522337b6f353ad0517a68cc36e0bf4d
bouchentouf <fatimazoh86@gmail.com>
2024-07-10 15:02:34 +0200

    first commit

---
+++ templets/header.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>zoo arcadia ecologique energie renouvlable protrction nature </title>
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Libre+Caslon+Text:ital,wght@0,400;0,700;1,400&display=swap" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="assets/style/override-bootstrap.css">
+    <link rel="stylesheet" href="assets/style/style.css">
+</head>
+<body class="libre-caslon-text-regular flux">
+    <!--debut header-->
+    <header class=" navcontainer d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-3 mb-4 border-bottom">
+        <div class="col-md-3 mb-2 mb-md-0">
+          <a href="/" class="d-inline-flex link-body-emphasis text-decoration-none">
+            <img src="assets/images/logoArcadia.jpg" alt="logo arcadia" width="150px" height="150px">
+          </a>
+        </div>
+  
+        <ul class="nav col-12 col-md-auto mb-2 justify-content-center mb-md-0">
+          <li><a href="#" class="nav-link px-2 link-secondary">Home</a></li>
+          <li><a href="#" class="nav-link px-2">habitats</a></li>
+          <li><a href="#" class="nav-link px-2">services</a></li>
+          <li><a href="#" class="nav-link px-2">contacte</a></li>
+        </ul>
+  
+        <div class="col-md-3 text-end">
+          <button type="button" class="btn btn-outline-primary me-2">Login</button>
+          <button type="button" class="btn btn-primary">logout</button>
+        </div>
+      </header>
+     <!--fin header--> 
---
+++ templets/page/home.php
@@ -0,0 +1,160 @@
+<?php require_once _ROOTPATH_.'\templates\header.php'; ?>
+
+<main>
+<!--section1 description du zoo-->  
+    <section>
+        <div class="opacity-75">
+            <img class="bd-placeholder-img rounded" width="100%" height="100%" src="assets/images/imageDescription_écolo.webp" aria-hidden="true" preserveAspectRatio="xMidYMid slice" focusable="false">
+            <div class="container">
+              <div class=" carousel-caption text-start">
+              <h1 class="text-uppercase">zoo arcadia</h1>
+                <p class="">Situé en France près de la forêt de Brocéliande, en Bretagne depuis 1960, ARCADIA</p>
+                
+              </div>
+            </div>
+    </section>
+<!--fin section1 -->
+
+<!--section2 habitas-->
+
+        <section>
+            <div class= 'my-5 mx-4'>
+                <div id="myCarousel" class="carousel slide mb-6" data-bs-ride="carousel">
+                    <div class="carousel-indicators">
+                      <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="0" class="" aria-label="Slide 1"></button>
+                      <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="1" aria-label="Slide 2" class="active" aria-current="true"></button>
+                      <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="2" aria-label="Slide 3" class=""></button>
+                    </div>
+                    <div class="carousel-inner ">
+                      <div class="carousel-item">
+                        <img class="bd-placeholder-img rounded" width="100%" height="100%" src="uploads/habitats/savaneAnimeaux.jpg" aria-hidden="true" preserveAspectRatio="xMidYMid slice" focusable="false"><rect width="100%" height="100%" fill="var(--bs-secondary-color)"></rect></svg>
+                        <div class="container">
+                          <div class="carousel-caption text-start">
+                            <h1 class="text-uppercase">savane</h1>
+                            <p class="opacity-75">meilleur service pour une remise a neuf de votre voiture</p>
+                            <p><a class="btn btn-lg btn-primary" href="services.php">visite des animeaux</a></p>
+                          </div>
+                        </div>
+                      </div>
+                      <div class="carousel-item active">
+                        <img class="bd-placeholder-img rounded" width="100%" height="100%" src="uploads/habitats/jungleAnimaux.jpg" aria-hidden="true" preserveAspectRatio="xMidYMid slice" focusable="false"><rect width="100%" height="100%" fill="var(--bs-secondary-color)"></rect></svg>
+                        <div class="container">
+                          <div class="carousel-caption">
+                            <h1 class="text-uppercase">jungle</h1>
+                            <p>notre garage vous offre le meilleur entretien pour vos voirure</p>
+                            <p><a class="btn btn-lg btn-primary" href="services.php">visite des animeaux</a></p>
+                          </div>
+                        </div>
+                      </div>
+                      <div class="carousel-item">
+                        <img class="bd-placeholder-img rounded" width="100%" height="100%" src="uploads/habitats/animeauMarais.jpg" aria-hidden="true" preserveAspectRatio="xMidYMid slice" focusable="false"><rect width="100%" height="100%" fill="var(--bs-secondary-color)"></rect></svg>
+                        <div class="container">
+                          <div class="carousel-caption text-end">
+                            <h1 class="text-uppercase">marais</h1>
+                            <p>pieces de qualité et employé qualifier pour la reparation de vos voiture</p>
+                            <p><a class="btn btn-lg btn-primary" href="">visite des animeaux</a></p>
+                          </div>
+                        </div>
+                      </div>
+                    
+                    <button class="carousel-control-prev" type="button" data-bs-target="#myCarousel" data-bs-slide="prev">
+                      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                      <span class="visually-hidden">Previous</span>
+                    </button>
+                    <button class="carousel-control-next" type="button" data-bs-target="#myCarousel" data-bs-slide="next">
+                      <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                      <span class="visually-hidden">Next</span>
+                    </button>
+                  </div>
+            </div>
+        </section>
+            <!-- fin section2-->
+       
+   
+ <!--fin des habitas-->
+
+    <!--section images animals-->
+    <section>
+       <h1 class="my=5">mettre des cartes pour quelque animeau</h1>
+    </section>
+    <!--fin section fin images animals-->
+
+    <!--section services-->
+    <section>
+        <div class="container marketing">
+
+            <!-- Three columns of text below the carousel -->
+            <div class="row">
+              <div class="col-lg-4">
+                <img class="bd-placeholder-img rounded-circle" width="140" height="140" src="uploads/services/restauration.webp" role="img" aria-label="Placeholder" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="var(--bs-secondary-color)"></rect>
+                <h2 class="fw-normal">restauration</h2>
+                <p>Some representative placeholder content for the three columns of text below the carousel. This is the first column.</p>
+                <p><a class="btn btn-secondary" href="#">View details »</a></p>
+              </div><!-- /.col-lg-4 -->
+              <div class="col-lg-4">
+                <img class="bd-placeholder-img rounded-circle" width="140" height="140" src="uploads/services/visite-guidee.jpg" role="img" aria-label="Placeholder" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="var(--bs-secondary-color)"></rect>
+                <h2 class="fw-normal">visite habitats</h2>
+                <p>Another exciting bit of representative placeholder content. This time, we've moved on to the second column.</p>
+                <p><a class="btn btn-secondary" href="#">View details »</a></p>
+              </div><!-- /.col-lg-4 -->
+              <div class="col-lg-4">
+                <img class="bd-placeholder-img rounded-circle" width="140" height="140" src="uploads/services/visiteTrain.jpg" role="img" aria-label="Placeholder" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="var(--bs-secondary-color)"></rect>
+                <h2 class="fw-normal">visite zoo</h2>
+                <p>And lastly this, the third column of representative placeholder content.</p>
+                <p><a class="btn btn-secondary" href="#">View details »</a></p>
+              </div><!-- /.col-lg-4 -->
+            </div><!-- /.row -->
+        
+        
+            
+          </div>
+    </section>
+    <!--fin section services-->
+
+ 
+
+    <!--section avis-->
+    <section>
+        <div class="container mb-5 pt-5">
+            <div class="row">
+              <div class="col-12 col-sm-7 col-md-12 m-auto">
+                <div class="card border-0 shadow">
+                  <div class="card-body container">
+                    <div class="mx-4 text-center ">
+                      <h3>laissez nous votre avis</h3>
+                      <b><h5>votre satisfaction et notres objectif veuiller nous laisser votre avis sur votre experience dans notre zoo</h5>
+                    </div>
+                    <div >  
+                      <div class="text-center my-4 " >
+                        <a href="formulaireAvis.php"><button class="btn btn-primary " type="button">poster votre avis</button></a>
+                      </div>
+                    </div>  
+                  </div>
+                </div>
+              </div>
+            </div>
+    </div>           
+    
+
+    </section>
+    <!--fin section avis-->
+
+
+
+  </main>
+
+<!--footer -->
+
+
+
+
+
+
+<?php require_once _ROOTPATH_.'\templates\footer.php'; ?>
+<!--fin footer-->
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+
+</body>
+
+</html>
